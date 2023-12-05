b7891093d6116da0dadfce07f969c598833a5b88
ville.nevasuo <ville.nevasuo@op.fi>
2023-12-05 13:25:46 +0200

    complete day5p1

--- 2023/day5/day5.py
+++ 2023/day5/day5.py
@@ -1,60 +1,48 @@
 
 
+def compute_mapping_for_value(source, map_data):
+    for coords in map_data:
+        destination, src_start, range_length = coords[0], coords[1], coords[2]
+        if src_start <= source < src_start + range_length:
+            return destination + (source - src_start)
+    return source
+
 def p1():
-
-
     data_dict = {}
-    
-    map_data_structures = {
-        'seed-to-soil map': {},
-        'soil-to-fertilizer map': {},
-        'fertilizer-to-water map': {},
-        'water-to-light map': {},
-        'light-to-temperature map': {},
-        'temperature-to-humidity map': {},
-        'humidity-to-location map': {}
-    }
+    current_map_name = None  # Initialize current_map_name before the loop
 
-    current_map_name = None
     with open("input.txt") as file:
-        for coords in file:
-            coords = coords.strip()
-            if 'seeds' in coords:
-                seeds = coords.split(":")[0].strip()
-                seednumbers = coords.split(":")[1].strip()
+        for line in file:
+            line = line.strip()
+            if 'seeds' in line:
+                seeds = line.split(":")[0].strip()
+                seednumbers = line.split(":")[1].strip()
                 data_dict[seeds] = list(map(int, seednumbers.split(" ")))
-            if 'map' in coords:
-                current_map_name = coords.split(":")[0].strip()
+            elif 'map' in line:
+                current_map_name = line.split(":")[0].strip()
                 data_dict[current_map_name] = []
-            elif current_map_name and coords:
-                map_data = list(map(int, coords.split()))
+            elif current_map_name and line:
+                map_data = list(map(int, line.split()))
                 data_dict[current_map_name].append(map_data)
-    
-    
-    print(data_dict)
-    for map_name, values in data_dict.items():
-        print(f'processing {map_name}')
-        if type(values[0]) is list:
-            for coords in values:
-                source, destination, range_length = coords[1], coords[0], coords[2]
-                for i in range(range_length):
-                    map_data_structures[map_name][source + i] = destination + i
-    
-    seednumbers = [int(num) for num in seednumbers.split(" ")]
 
+    seednumbers = data_dict.get('seeds', [])
 
     locations = []
 
     for seed in seednumbers:
         source = seed
-        for map_name in map_data_structures.keys():
-            current_map = map_data_structures[map_name]
-            source = current_map.get(source, source)
+        for map_name in ['seed-to-soil map', 'soil-to-fertilizer map', 'fertilizer-to-water map',
+                         'water-to-light map', 'light-to-temperature map', 'temperature-to-humidity map',
+                         'humidity-to-location map']:
+            map_data = data_dict.get(map_name, [])
+            source = compute_mapping_for_value(source, map_data)
 
         locations.append(source)
         print(f"Final value for seed {seed} is {source}")
 
     return min(locations)
+
+
 
 def p2():
     
