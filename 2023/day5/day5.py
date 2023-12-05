ffeac34b980c22106bdeb85d73a6266458f369e2
ville.nevasuo <ville.nevasuo@op.fi>
2023-12-05 14:37:46 +0200

    add day5 stuff

--- 2023/day5/day5.py
+++ 2023/day5/day5.py
@@ -9,7 +9,7 @@ def compute_mapping_for_value(source, map_data):
 
 def p1():
     data_dict = {}
-    current_map_name = None  # Initialize current_map_name before the loop
+    current_map_name = None
 
     with open("input.txt") as file:
         for line in file:
@@ -45,9 +45,41 @@ def p1():
 
 
 def p2():
-    
-    return 0
+    data_dict = {}
+    current_map_name = None
+
+    with open("input.txt") as file:
+        for line in file:
+            line = line.strip()
+            if 'seeds' in line:
+                _, seed_ranges_str = line.split(":")
+                seed_ranges = seed_ranges_str.strip().split()
+            elif 'map' in line:
+                current_map_name = line.split(":")[0].strip()
+                data_dict[current_map_name] = []
+            elif current_map_name and line:
+                map_data = list(map(int, line.split()))
+                data_dict[current_map_name].append(map_data)
+
+    locations = []
+
+    for i in range(0, len(seed_ranges), 2):
+        start = int(seed_ranges[i])
+        length = int(seed_ranges[i + 1])
+        for seed in range(start, start + length):
+            source = seed
+            for map_name in ['seed-to-soil map', 'soil-to-fertilizer map', 'fertilizer-to-water map',
+                             'water-to-light map', 'light-to-temperature map', 'temperature-to-humidity map',
+                             'humidity-to-location map']:
+                map_data = data_dict.get(map_name, [])
+                source = compute_mapping_for_value(source, map_data)
+
+            locations.append(source)
+
+    return min(locations)
 
 
-print(p1())
-#print(p2())
+
+
+#print(p1())
+print(p2())
